ea1f04fc4931047f7dd5d6f087aab2cd55f72074
Muhammad Maina <moh445474@gmail.com>
2025-09-29 13:44:14 +0100

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-     outputFileTracingRoot: 'C:\Users\moode\Documents\versevault'  
-};
+const nextConfig = {};
 
 export default nextConfig;
